f43873b36557ab4d00d86baeb07ee675d50bb764
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-06-09 10:19:48 +0530

    Add expect handlers in test cases

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,11 @@
-// const expect = require('chai').expect;
+const expect = require('chai').expect;
 const { readConf } = require('@gh-conf/gh-conf-read');
 const { writeConf } = require('@gh-conf/gh-conf-write');
 
 const { Upstreamit } = require('../lib');
+
+
+const UPSTREAM_CONFIG = '[remote "upstream"]\n\turl = https://github.com/taskcluster/taskcluster\n\tfetch = +refs/heads/*:refs/remotes/upstream/*\n';
 
 
 describe('validate upstreaminit', () => {
@@ -11,9 +14,13 @@ describe('validate upstreaminit', () => {
     let modifiedConfig = config.replace('https://github.com/gh-conf/upstreamit', 'https://github.com/arshadkazmi42/taskcluster');
     await writeConf(process.cwd(), modifiedConfig);
     await Upstreamit(process.cwd());
+    expect((await readConf(process.cwd())).includes(UPSTREAM_CONFIG)).to.equal(true);
+
+    // Reset Original Config
     await writeConf(process.cwd(), config);
   });
-  it('upstream show throw error not a forked repository', async () => {
+  it('upstream throws error not a forked repository', async () => {
     await Upstreamit(process.cwd());
+    expect((await readConf(process.cwd())).includes(UPSTREAM_CONFIG)).to.equal(false);
   });
 });
